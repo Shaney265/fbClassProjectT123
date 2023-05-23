08b722e7547abcab460cce5357c933ed26dd6c38
shandel <shandelskulsky@gmail.com>
2023-05-23 20:45:23 +1000

    Display title of 3 books

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,50 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import { FBDBContext } from '../contexts/FBDBContext';
-
+import { useContext, useEffect, useState } from 'react';
+import {collection, doc, getDocs} from "firebase/firestore";
 
 export function Home () {
+    const [data, setData ]= useState([])
+    const FBDB = useContext(FBDBContext)
+
+    const getData = async() => {
+//get data from the firebase collection called books
+        const querySnapshot = await getDocs(collection(FBDB, "books"))
+ // an array to store all books in firestore  
+        let books = []
+        querySnapshot.forEach( (doc) => {
+            let book = doc.data()
+            book.id = doc.id
+ //add book to th array           
+            books.push(book)
+
+            
+        })
+// set the books array as the data state        
+        setData(books)
+        console.log (books)
+    }
+ useEffect(() => {
+    if(data.length === 0) {
+        getData()
+    }
+ })
+ const Columns = data.map( (book, key) =>{
+    return(
+        <Col md="4" key={key}>
+            <h3>{book.title}</h3>
+        </Col>
+    )
+ })
+ 
+    return (
+        <Container>
+            <Row>
+                {Columns}
+            </Row>
+        </Container>
+    )
     return (
         <div>
             <h1>Home</h1>
