6d104f6cda9dca2a992bd67e29b24e27fc6d971c
shandel <shandelskulsky@gmail.com>
2023-06-06 18:19:33 +1000

    Added my firebase code

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,9 +1,11 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBRLU8My3cFx7Q9j05RlpGcADE2jSpHKvs",
-    authDomain: "classproject-937e8.firebaseapp.com",
-    projectId: "classproject-937e8",
-    storageBucket: "classproject-937e8.appspot.com",
-    messagingSenderId: "669600973558",
-    appId: "1:669600973558:web:72e86eca25137e80d622d7"
+    apiKey: "AIzaSyC9aZNRBk5QCen6h8bU_zn_CRDnlR11VrQ",
+    authDomain: "myfirstclassproject.firebaseapp.com",
+    projectId: "myfirstclassproject",
+    storageBucket: "myfirstclassproject.appspot.com",
+    messagingSenderId: "582338962333",
+    appId: "1:582338962333:web:6b2928f27717fd5eee4574"
 };
+//connect this code to my firebase project account
+
   
